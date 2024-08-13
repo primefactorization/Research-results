dee21651111e97423f16a4a7a8d69642546709dd
primefactorization <nisikyokusa@gmail.com>
2024-08-13 12:07:59 +0900

    Update make random numbers.py

--- 乱数/make random numbers.py
+++ 乱数/make random numbers.py
@@ -1,1 +1,10 @@
+import random
 
+# ◯桁の乱数を10個生成（重複なし）
+random_numbers = []
+while len(random_numbers) < 10:
+    num = random.randint(1000000000000000000000000000000000000000, 9999999999999999999999999999999999999999)
+    if num not in random_numbers:
+        random_numbers.append(num)
+
+print(random_numbers)
