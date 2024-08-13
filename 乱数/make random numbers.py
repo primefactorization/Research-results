109ea35596969630c411dde7f136644b45420d1e
primefactorization <nisikyokusa@gmail.com>
2024-08-13 15:38:09 +0900

    Update make random numbers.py

--- 乱数/make random numbers.py
+++ 乱数/make random numbers.py
@@ -1,9 +1,12 @@
 import random
 
+number_len =#ここに数字の桁数を入力
+star = 10 ** number_len
+end = 10 ** (number_len + 1)
 # ◯桁の乱数を10個生成（重複なし）
 random_numbers = []
 while len(random_numbers) < 10:
-    num = random.randint(1000000000000000000000000000000000000000, 9999999999999999999999999999999999999999)
+    num = random.randint(start, end)
     if num not in random_numbers:
         random_numbers.append(num)
 
