fd78893530975ebd1d6b63f2bb421ecfc02c720a
primefactorization <nisikyokusa@gmail.com>
2024-08-14 14:43:01 +0900

    Update prime.py

--- 統合プログラムの作成/prime.py
+++ 統合プログラムの作成/prime.py
@@ -155,27 +155,23 @@ prime_file = "sosuu.gz"
 prime_list = decompress_and_decode_gzip_with_timing(prime_file)
 
 
-def a(numbers):
-    # 結果を格納するリスト
-    results = []
-    execution_times = []
-    # リスト内の各数について素因数分解を行う（複数回実行）
-    for num in numbers.split(","):
-        num = int(num)
-        start_time = time.perf_counter()
-        factors = send_program(num)
-        end_time = time.perf_counter()
-        execution_time = (end_time - start_time) * 1000  # ミリ秒単位
-        results.append(factors)
-        execution_times.append(execution_time)
-    overall_average_time = sum(execution_times) / len(execution_times)
+# 数値のリスト
+numbers = []
 
-    # 結果をファイルに保存
-    with open("results.txt", "a") as f:
-        f.write(f"\n{len(str(num))}  桁の全体の平均実行時間: {overall_average_time:.6f} ミリ秒\n")
+# 結果を格納するリスト
+results = []
+execution_times = []
 
-with open("ti.txt","r") as f:
-    zennbu = f.read().split("\n")
-for li in zennbu:
-    a(li)
-    time.sleep(1)
+# リスト内の各数について素因数分解を行う（複数回実行）
+for num in numbers:
+    start_time = time.perf_counter()
+    factors = send_program(num)
+    end_time = time.perf_counter()
+    execution_time = (end_time - start_time) * 1000  # ミリ秒単位
+    results.append(factors)
+    execution_times.append(execution_time)
+overall_average_time = sum(execution_times) / len(execution_times)
+
+# 結果をファイルに保存
+with open("results.txt", "a") as f:
+    f.write(f"\n{len(str(numbers[1]))}  桁の全体の平均実行時間: {overall_average_time:.6f} ミリ秒\n")
